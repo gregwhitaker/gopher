a33a8fcb6129208da75d30f540a72fa4a40b5b7e
gregwhitaker <gwhitake@gmail.com>
2020-04-04 19:43:25 -0700

    Doing work

--- gopher-server/src/main/java/com/gopherlinks/server/service/GoLinkResolverService.java
+++ gopher-server/src/main/java/com/gopherlinks/server/service/GoLinkResolverService.java
@@ -1,17 +1,68 @@
 package com.gopherlinks.server.service;
 
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Component
 public class GoLinkResolverService {
     private static final Logger LOG = LoggerFactory.getLogger(GoLinkResolverService.class);
 
+    private final AsyncLoadingCache<String, String> resolverCache;
+
+    public GoLinkResolverService() {
+        this.resolverCache = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterAccess(Duration.ofHours(24))
+                .buildAsync(this::loadKey);
+
+        // Populate cache with all active GoLinks in the datastore
+        loadAll()
+                .publishOn(Schedulers.boundedElastic())
+                .subscribe();
+    }
+
+    /**
+     * Resolves the supplied GoLink to a URL.
+     *
+     * @param goLink GoLink to resolve
+     * @return resolved URL
+     */
     public Mono<String> resolveGoLink(String goLink) {
-        return Mono.fromSupplier(() -> {
-            return "https://www.google.com";
+        return Mono.fromFuture(resolverCache.get(goLink));
+    }
+
+    /**
+     * Loads a single GoLink into the cache asynchronously.
+     *
+     * @param key cache key to load
+     * @param executor executor on which to run the cache load
+     * @return resolved GoLink value to cache
+     */
+    private CompletableFuture<String> loadKey(String key, Executor executor) {
+        return CompletableFuture.supplyAsync(() -> {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Key not found in cache! Loading from datastore [key: '{}']", key);
+            }
+
+            return "http://www.google.com";
+        }, executor);
+    }
+
+    /**
+     * Loads the cache with all GoLinks in the datastore.
+     */
+    private Mono<Void> loadAll() {
+        return Mono.defer(() -> {
+           return Mono.empty();
         });
     }
 }
