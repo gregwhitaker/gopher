96cd814b7830e4a9807638ad4477b2cc65717d85
gregwhitaker <gwhitake@gmail.com>
2020-04-04 19:13:27 -0700

    Doing work

--- gopher-server/src/main/java/com/gopherlinks/server/controller/GoLinkResolverController.java
+++ gopher-server/src/main/java/com/gopherlinks/server/controller/GoLinkResolverController.java
@@ -4,6 +4,8 @@ import com.gopherlinks.server.service.GoLinkResolverService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +24,11 @@ public class GoLinkResolverController {
     }
 
     @GetMapping(value = "/{golink}")
-    public Mono<ResponseEntity<?>> resolveGoLink(@PathVariable("goLink") String goLink) {
-        return null;
+    public Mono<ResponseEntity<Object>> resolveGoLink(@PathVariable("golink") String goLink) {
+        return goLinkResolverService.resolveGoLink(goLink)
+                .map(redirectUrl -> ResponseEntity.status(HttpStatus.SEE_OTHER)
+                        .header(HttpHeaders.LOCATION, redirectUrl)
+                        .build())
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 }
--- gopher-server/src/main/java/com/gopherlinks/server/service/GoLinkResolverService.java
+++ gopher-server/src/main/java/com/gopherlinks/server/service/GoLinkResolverService.java
@@ -3,9 +3,15 @@ package com.gopherlinks.server.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class GoLinkResolverService {
     private static final Logger LOG = LoggerFactory.getLogger(GoLinkResolverService.class);
 
+    public Mono<String> resolveGoLink(String goLink) {
+        return Mono.fromSupplier(() -> {
+            return "https://www.google.com";
+        });
+    }
 }
