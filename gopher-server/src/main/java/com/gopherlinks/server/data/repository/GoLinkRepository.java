231601d226f80db98c2bde287a21c388611467f8
gregwhitaker <gwhitake@gmail.com>
2020-04-05 12:59:02 -0700

    Doing work

--- gopher-server/src/main/java/com/gopherlinks/server/data/repository/GoLinkRepository.java
+++ gopher-server/src/main/java/com/gopherlinks/server/data/repository/GoLinkRepository.java
@@ -9,13 +9,44 @@ import java.util.concurrent.Executor;
 
 public interface GoLinkRepository {
 
+    /**
+     * Returns the redirect url for a GoLink.
+     *
+     * @param goLink golink
+     * @return redirect url
+     */
     Mono<String> findOne(String goLink);
 
+    /**
+     * Returns the redirect url for a GoLink.
+     *
+     * @param goLink golink
+     * @param executor executor on which to run the query
+     * @return redirect url
+     */
     CompletableFuture<String> findOne(String goLink, Executor executor);
 
+    /**
+     * Returns all GoLinks.
+     *
+     * @return all golinks
+     */
     Flux<Tuple2<String, String>> findAll();
 
+    /**
+     * Saves a GoLink.
+     *
+     * @param goLink golink
+     * @param url redirect url
+     * @return void
+     */
     Mono<Void> put(String goLink, String url);
 
+    /**
+     * Removes a GoLink.
+     *
+     * @param goLink golink
+     * @return void
+     */
     Mono<Void> remove(String goLink);
 }
